945cbef51c48f19690f6255d4527d5808f6f097c
Ugne Staniute <u.staniute@gmail.com>
2018-05-30 11:31:04 +0300

    A bit of fixing & optimization.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,50 @@
 $(function() {
-    window.onscroll = function() {
-        navHook();
-        btnHook();
-        navigator();
-    };
 
-    var $header = $('header');
-    var $navbar = $('#nav-bar');
-    var $nHome = $('#nHome');
-    var $nAbout = $('#nAbout');
-    var $nBlog = $('#nBlog');
-    var $nTeam = $('#nTeam');
-    var $nContact = $('#nContact');
-    var $pagelinks = $('.navigation');
-    var $hamburger = $('.socials .hamburger');
-    var navLinks = [$nHome, $nAbout, $nBlog, $nTeam, $nContact];
-    var $scrollButton = $('.scroll-btn');
-    var $arrow = $('.scroll-btn i');
-    var $sAbout = $('.about');
-    var $sBlog = $('.blog');
-    var $sTeam = $('.team');
-    var $sContact = $('.contact');
-    var $form = $('#cForm');
+    const $header = $('header');
+    const $navbar = $('#nav-bar');
+    const $home = $('#nHome');
+    const $about = $('#nAbout');
+    const $blog = $('#nBlog');
+    const $team = $('#nTeam');
+    const $contact = $('#nContact');
+    const $sAbout = $('.about');
+    const $sBlog = $('.blog');
+    const $sTeam = $('.team');
+    const $sContact = $('.contact');
+    const $sFooter = $('footer');
+    const sectionCoord = [
+        [() => {return $home}, () => {return 0}],
+        [() => {return $about}, () => {return $sAbout.position().top}],
+        [() => {return $blog}, () => {return $sBlog.position().top}],
+        [() => {return $team}, () => {return $sTeam.position().top}],
+        [() => {return $contact}, () => {return $sContact.position().top}],
+        [() => {return null}, () => {return $sFooter.position().top}]
+    ];
+    let lastChangeIndex = 0;
+    const $pageLinks = $('.navigation');
+    const $hamburger = $('.socials .hamburger');
+    const $scrollButton = $('.scroll-btn');
 
-    var sticky = $navbar.offset().top;
-    var btnSticky = $scrollButton.offset().top;
+    const form  = document.getElementById('cForm');
+    const name = document.getElementById('cName');
+    const nameError = name.nextElementSibling;
+    const email = document.getElementById('cEmail');
+    const emailError = email.nextElementSibling;
+    const phone = document.getElementById('cPhone');
+    const message = document.getElementById('cMessage');
+    const messageError = message.nextElementSibling;
+    const nameRegExp = /[a-zA-Z][^#&<>\"~;$^%{}?]{1,20}$/;
+    const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    const sticky = $navbar.offset().top;
+    const btnSticky = $scrollButton.offset().top;
 
     function navHook() {
-        if ((window.pageYOffset >= sticky) && (!$navbar.hasClass('navColumn'))) {
+        if (!$navbar.hasClass('navColumn') && window.pageYOffset >= sticky) {
             $navbar.addClass("sticky")
         } else {
             $navbar.removeClass("sticky")
         }
-    };
-
-    function btnHook() {
         if (window.pageYOffset >= btnSticky) {
             $scrollButton.addClass("btnSticky")
         } else {
@@ -43,140 +53,140 @@ $(function() {
     };
 
     function navigator() {
-        var aboutPosition = $sAbout.position().top - 360;
-        var blogPosition = $sBlog.position().top - 360;
-        var teamPosition = $sTeam.position().top - 360;
-        var contactPosition = $sContact.position().top - 360;
-        if (scrollY < (aboutPosition)) {
-            $.each(navLinks, function(index, value) {
-                value.removeClass('active');
-            });
-            $nHome.addClass('active');
-        } else if (scrollY >= (aboutPosition) && scrollY < blogPosition) {
-            $.each(navLinks, function(index, value) {
-                value.removeClass('active');
-            });
-            $nAbout.addClass('active');
-        } else if (scrollY >= blogPosition && scrollY < teamPosition) {
-            $.each(navLinks, function(index, value) {
-                value.removeClass('active');
-            });
-            $nBlog.addClass('active');
-        } else if (scrollY >= teamPosition && scrollY < contactPosition) {
-            $.each(navLinks, function(index, value) {
-                value.removeClass('active');
-            });
-            $nTeam.addClass('active');
-        } else if (scrollY >= contactPosition) {
-            $.each(navLinks, function(index, value) {
-                value.removeClass('active');
-            });
-            $nContact.addClass('active');
-        }
-        if (scrollY >= contactPosition) {
-            $arrow.removeClass('fa-angle-down');
-            $arrow.addClass('fa-angle-up');
-        } else {
-            $arrow.removeClass('fa-angle-up');
-            $arrow.addClass('fa-angle-down');
+        let i = 1;
+        let eLength = sectionCoord.length;
+        let currentPosition = scrollY;
+        for ( ; i < eLength; i++) {
+            if (currentPosition < (sectionCoord[i][1]()  - 360)) {
+                if (i !== lastChangeIndex) {
+                    $.each(sectionCoord, function(index, value) {
+                        if (!value[0]()) {return;};
+                        value[0]().removeClass('active');
+                    });
+                    sectionCoord[i-1][0]().addClass('active');
+                    lastChangeIndex = i;
+                }
+                return;
+            }
+            if (lastChangeIndex > 4) {
+                $scrollButton.hide();
+            } else {
+                $scrollButton.show();
+            }
         }
     };
 
-    $hamburger.on('click', function() {
-        $(this).toggleClass('open');
+    function scrollToSection(event, index) {
+        event.preventDefault();
+        window.scrollTo({
+            top: sectionCoord[index][1](),
+            behavior: "smooth",
+            onAfter: navigator()
+        });
+    }
+    
+    function toggleHamburger() {
+        $hamburger.toggleClass('open');
         $navbar.toggleClass('navColumn');
-        $pagelinks.toggleClass('shown');
+        $pageLinks.toggleClass('shown');
         $header.toggleClass('navPadding');
         navHook();
+    }
+
+    $hamburger.on('click', function() {
+        toggleHamburger();
+    });
+    
+    $scrollButton.on('click', function(event) {
+        scrollToSection(event, lastChangeIndex);
     });
 
-    $scrollButton.on('click', function(event) {
-        event.preventDefault();
-        var aboutPosition = $sAbout.position().top;
-        var blogPosition = $sBlog.position().top;
-        var teamPosition = $sTeam.position().top;
-        var contactPosition = $sContact.position().top;
-        if (scrollY < (aboutPosition)) {
-            window.scrollTo({
-                top: aboutPosition + 1,
-                behavior: "smooth",
-                onAfter: navigator()
-            });
-        } else if (scrollY >= (aboutPosition) && scrollY < blogPosition) {
-            window.scrollTo({
-                top: $sBlog.position().top + 1,
-                behavior: "smooth",
-                onAfter: navigator()
-            });
-        } else if (scrollY >= blogPosition && scrollY < teamPosition) {
-            window.scrollTo({
-                top: $sTeam.position().top + 1,
-                behavior: "smooth",
-                onAfter: navigator()
-            });
-        } else if (scrollY >= teamPosition && scrollY < contactPosition) {
-            window.scrollTo({
-                top: $sContact.position().top + 1,
-                behavior: "smooth",
-                onAfter: navigator()
-            });
-        } else if (scrollY >= contactPosition) {
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-                onAfter: navigator()
-            });
+    $.each(sectionCoord, function(index, value) {
+        if (!value[0]()) {return;};
+        value[0]().on('click', function(event) {
+            scrollToSection(event, index);
+            if ($hamburger.hasClass('open')) {
+                toggleHamburger();
+            }
+        });
+    });
+    
+    window.onscroll = function() {
+        navHook();
+        navigator();
+    };
+
+    $(window).resize(function(){
+        if ($hamburger.hasClass('open') && $(window).width() > 900){
+            toggleHamburger();
         }
     });
-
-    $nHome.on('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-            onAfter: navigator()
-        });
-    });
-
-    $nAbout.on('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: $sAbout.position().top,
-            behavior: "smooth",
-            onAfter: navigator()
-        });
-    });
-
-    $nBlog.on('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: $sBlog.position().top,
-            behavior: "smooth",
-            onAfter: navigator()
-        });
-    });
-
-    $nTeam.on('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: $sTeam.position().top,
-            behavior: "smooth",
-            onAfter: navigator()
-        });
-    });
-
-    $nContact.on('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({
-            top: $sContact.position().top,
-            behavior: "smooth",
-            onAfter: navigator()
-        });
-    });
-
-    navHook();
-    btnHook();
-    navigator();
     
-	$form.validate();
+    function addEvent(element, event, callback) {
+        let previousEventCallBack = element["on"+event];
+        element["on"+event] = function (e) {
+            let output = callback(e);
+        
+            if (output === false) return false;
+        
+            if (typeof previousEventCallBack === 'function') {
+                output = previousEventCallBack(e);
+                if(output === false) return false;
+            }
+        }
+      };
+
+    function checkValidity(input, result) {
+        if (result) {
+            input.className = "form-cont valid";
+            input.nextElementSibling.innerHTML = "";
+            input.nextElementSibling.className = "error";
+        } else {
+            input.className = "form-cont invalid";
+        }
+    }
+
+    function errorDisplay(input, message) {
+        input.className = "form-cont invalid";
+        input.nextElementSibling.innerHTML = message;
+        input.nextElementSibling.className = "error active";
+    }
+      
+    name.addEventListener("input", function (event) {
+        let result = name.value.length !== 0 && nameRegExp.test(name.value);
+        checkValidity(this, result);
+      }, false);
+      
+    email.addEventListener("input", function (event) {
+        let result = email.value.length !== 0 && emailRegExp.test(email.value);
+        checkValidity(this, result);
+      }, false);
+      
+      message.addEventListener("input", function (event) {
+          let result = message.value.length !== 0;
+          checkValidity(this, result);
+        }, false);
+      
+    form.addEventListener("submit", function (event) {
+        let result = email.value.length !== 0 && emailRegExp.test(email.value) &&
+            name.value.length !== 0 && nameRegExp.test(name.value) &&
+            message.value.length !== 0;
+        if (!result) {
+            event.preventDefault();
+            if (name.value.length === 0 && !nameRegExp.test(name.value)) {
+                errorDisplay(name, "Give us a name for referral.")
+            }
+            if (email.value.length === 0 && !emailRegExp.test(email.value)) {
+                errorDisplay(email, "A valid email adress is required (__@__.__).")
+            }
+            if (message.value.length === 0) {
+                errorDisplay(message, "Please leave a message.")
+            }
+            phone.className = "form-cont valid";
+        }
+      }, false);
+
+      navHook();
+      navigator();
+      
 });
